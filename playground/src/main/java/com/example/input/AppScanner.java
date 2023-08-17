10f3f83c27c5bda3ed76295afccf458117492e96
Ailton Antônio de Araújo <ailton.a.araujo@gmail.com>
2023-08-17 22:44:10 +0000

    Trabalhando com Entrada de Dados pelo teclado

---
+++ playground/src/main/java/com/example/input/AppScanner.java
@@ -0,0 +1,21 @@
+package com.example.input;
+
+import java.util.Scanner;
+
+
+
+
+public class AppScanner {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um valor");
+        String nome = scanner.nextLine();
+
+        System.out.println("Valor digitado foi: "+nome);
+
+
+
+    }
+}
