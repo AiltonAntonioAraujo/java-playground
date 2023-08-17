968dcc4a12116a15a6f9c3d3895f603e71be573f
Ailton Antônio de Araújo <ailton.a.araujo@gmail.com>
2023-08-17 22:45:09 +0000

    resolvendo problema try-catch

--- playground/src/main/java/com/example/input/AppScanner.java
+++ playground/src/main/java/com/example/input/AppScanner.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 public class AppScanner {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite um valor");
+            String nome = scanner.nextLine();
 
-        System.out.println("Digite um valor");
-        String nome = scanner.nextLine();
-
-        System.out.println("Valor digitado foi: "+nome);
+            System.out.println("Valor digitado foi: "+nome);
+        }
 
 
 
